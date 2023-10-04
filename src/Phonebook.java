4ee51daf0dac5e029c621435b371dadd5971ad32
Abdullah Alothman <abode6185@gmail.com>
2023-10-04 21:40:02 +0300

    phonebook class add

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class Phonebook {
 
+    public boolean add() {
+        Scanner input = new Scanner(System.in);
+        {
+            Contact tmp = new Contact();
 
-    
+            System.out.println("Enter contact name: ");
+            String contactName = input.next();
+
+            System.out.println("Enter contact phone: ");
+            String contactPhone = input.next();
+
+            System.out.println("Enter contact email: ");
+            String contactEmail = input.next();
+
+            System.out.println("Enter contact address: ");
+            String contactAddress = input.next();
+
+            System.out.println("Enter contact birthday: ");
+            String contactBirthday = input.next();
+
+            System.out.println("Enter contact notes: ");
+            String contactNotes = input.next();
+
+
+            return true;
+        }
+    }
 }
