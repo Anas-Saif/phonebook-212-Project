03774bb202b30722d138056b272178cdf47ac15b
Mofj12 <Mohammed.aljalwi@gmail.com>
2023-10-14 13:33:18 +0300

    event class display and some changes

--- src/Event.java
+++ src/Event.java
@@ -4,11 +4,21 @@ public class Event {
     private String location;
     private Contact contact;
 
+    public Event() {
+    }
+
     public Event(String title, String dateAndTime, String location, Contact contact) {
         this.title = title;
         this.dateAndTime = dateAndTime;
         this.location = location;
         this.contact = contact;
+    }
+
+    public Event (Event e){
+        this.title = e.title;
+        this.dateAndTime = e.dateAndTime;
+        this.location = e.location;
+        this.contact = e.contact;
     }
 
     public String getTitle() {
@@ -25,6 +35,25 @@ public class Event {
 
     public Contact getContact() {
         return contact;
+    }
+
+
+    public void displayEvents(){
+        System.out.println("title: "+title);
+        System.out.println("date and time: "+dateAndTime);
+        System.out.println("location: "+location);
+        System.out.println("with: "+ contact.getContactName());
+    }
+
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "title='" + title + '\'' +
+                ", dateAndTime='" + dateAndTime + '\'' +
+                ", location='" + location + '\'' +
+                ", contact=" + contact +
+                '}';
     }
 }
 
